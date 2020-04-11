bdf87c3ef1c09a7da8a1f1675a0e31809cf9c8d0
Catalina <tech0w0@gmail.com>
2020-04-11 08:45:18 -0400

    Fifth try

--- app/routes.py
+++ app/routes.py
@@ -9,12 +9,14 @@ db = client.urls
 host = 'http://localhost:80/'
 table = db.links
 
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         data = request.form.get('url')
-        if data.split(':')[0] == 'https' or data.split(':')[0] == 'http':
+        if table.find_one({'long-url': data}):
+            result = table.find_one({'long-url': data})
+            return render_template('index.html', short_url=host + result["hash"])
+        elif data.split(':')[0] == 'https' or data.split(':')[0] == 'http':
             hash = hashlib.md5(data.encode()).hexdigest()
             post_data = {
                 'long-url': data,
@@ -22,10 +24,12 @@ def home():
             }
             table.insert_one(post_data)
             return render_template('index.html', short_url=host + hash[:5])
+        elif not data:
+            return render_template('index.html')
         else:
             if table.find_one({'hash': data}):
                 result = table.find_one({'hash': data})
-                return render_template('index.html', result["long-url"])
+                return render_template('index.html', short_url=result["long-url"])
             else:
                 return render_template('index.html', 'Hash is not in the DB')
     return render_template('index.html')
